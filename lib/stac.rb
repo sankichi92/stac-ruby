80412e1734d4570531ea14eebd7f7d95d2e6b2ba
Takahiro Miyoshi <takahiro-miyoshi@sankichi.net>
2022-09-09 21:17:19 +0900

    bundle gem stac -t rspec

---
+++ lib/stac.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "stac/version"
+
+module Stac
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/stac_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Stac do
+  it "has a version number" do
+    expect(Stac::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
